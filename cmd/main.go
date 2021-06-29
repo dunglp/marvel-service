22ac3e16daca35739eada80b86835e63aa8c3858
Bi <dunglp1303@gmail.com>
2021-06-30 04:29:14 +0700

    Remove debug stuffs

--- cmd/main.go
+++ cmd/main.go
@@ -10,10 +10,6 @@ import (
 )
 
 func main() {
-	os.Setenv("APP_HOST_NAME", "http://gateway.marvel.com")
-	os.Setenv("APP_PUBLIC_KEY", "5d04d11a2b5d49b6aa05a49998b5f083")
-	os.Setenv("APP_PRIVATE_KEY", "481732840d168b3244a9f2a4efb3c21d2b1ce2e0")
-	os.Setenv("APP_TS", "1")
 	defer func() {
 		if r := recover(); r != nil {
 			log.Printf("Service panicked %s: %s", r, string(debug.Stack()))
